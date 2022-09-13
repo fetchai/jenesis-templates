9fd2e1e64a3a0ffdec0552f824ffa493e9068c91
James Riehl <33920192+jrriehl@users.noreply.github.com>
2022-09-13 17:24:21 +0100

    fix: add name template to schema.rs (#1)
    * fix: add name template to schema.rs
    
    * chore: update schemas

--- contracts/starter/examples/schema.rs
+++ contracts/starter/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use starter::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg};
-use starter::state::State;
+use <<NAME>>::msg::{ExecuteMsg, GetCountResponse, InstantiateMsg, QueryMsg};
+use <<NAME>>::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();

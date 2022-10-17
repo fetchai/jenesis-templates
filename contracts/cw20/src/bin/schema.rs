d8720d0bf6d0ba4cabe8dff94f1da6d7c050d9f8
James Riehl <james.riehl@fetch.ai>
2022-10-17 15:53:08 +0100

    fix: add template name to schema src

--- contracts/cw20/src/bin/schema.rs
+++ contracts/cw20/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw20_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use <<NAME>>::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
